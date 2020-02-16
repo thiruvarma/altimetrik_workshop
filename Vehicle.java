b3e7e896d87fcbe0fea404e389a8819a8fa1f099
thiruvarma <thirupathivarma@gmail.com>
2020-02-16 13:04:07 -0500

    Add files via upload

--- Vehicle.java
+++ Vehicle.java
@@ -2,20 +2,6 @@ package com.altimetrik.workshop.vehicleservice.model;
 
 import java.util.LinkedList;
 
-//
-//<Count>142</Count>
-//<Message>
-//Results returned successfully. NOTE: Any missing decoded values should be interpreted as NHTSA does not have data on the specific variable. Missing value should NOT be interpreted as an indication that a feature or technology is unavailable for a vehicle.
-//</Message>
-//<SearchCriteria>VIN:2HKRM4H74FH695177</SearchCriteria>
-//<Results>
-//<DecodedVariable>
-//<VariableId>142</VariableId>
-//<Variable>Suggested VIN</Variable>
-//<ValueId/>
-//<Value/>
-//</DecodedVariable>
-
 public class Vehicle {
 	private LinkedList<VehicleDecodedVariable> vehicleVariableList;
 	private int count;
